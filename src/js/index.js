c91d21f677a04d3c7c9cb4818ffd18911e6259c2
Alejandro Corsino <me@alejandrocorsino.com>
2016-09-12 21:40:58 -0700

    Image tiles side to side

--- src/js/index.js
+++ src/js/index.js
@@ -3,10 +3,29 @@
 $(function(){
    require('animate.css/animate.css');
   // var logger = new (require('./util/Logger'))();
-  // var masongrid = require('./masongrid');
 
   // logger.debug('Page ready!');
   // var Vivus = require('vivus');
-  // var v = new Vivus('my-svg-id');
-  // logger.debug(v);
+  setImagesRows();
+
+
+  function setImagesRows(){
+    $('.pic-row').each(function(){
+      var ratios = 0;
+      $(this).children().each(function(){
+        ratios += getImgRatio($(this));
+      });
+      var h= $(document).width()*0.93/ratios ;
+      $(this).height(h);
+    });
+  };
+
+  function getImgRatio(e){
+    return e[0].naturalWidth / e[0].naturalHeight;
+  };
+
+  $( window ).resize(function() {
+    setImagesRows(); //TODO throttle
+  });
+
 });
