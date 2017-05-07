6d491f16a779faec12a71f8dc96d7bcb1af5fa3a
Alejandro Corsino <me@alejandrocorsino.com>
2017-05-07 15:04:07 -0700

    Cache control - gustil

--- gs-deploy.sh
+++ gs-deploy.sh
@@ -4,8 +4,12 @@
 
 echo Syncing files... [www.corsinoart.com]
 echo -- -- --
-gsutil rsync -d -r www/ gs://www.corsinoart.com
+gsutil -m rsync -d -r www/ gs://www.corsinoart.com
 
 echo Enabling public-read access to all files...
 echo -- -- --
 gsutil -m acl set -R -a public-read gs://www.corsinoart.com
+
+echo Cache Control to all Static pictures...
+echo -- -- --
+gsutil -m setmeta -h "Cache-Control:public, max-age=31536000"  gs://www.corsinoart.com/assets/pics/*.jpg
